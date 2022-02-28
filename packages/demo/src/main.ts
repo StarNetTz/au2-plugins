1859fa999717d426cde66e4b65de3dee854face3
Zvjezdan Tomicevic <zeko77@gmail.com>
2022-02-28 09:06:55 +0100

    Progress

--- packages/demo/src/main.ts
+++ packages/demo/src/main.ts
@@ -6,17 +6,8 @@ import { AureliaAuthConfiguration, IAuthConfigOptions, Authentication } from '@s
 
 Aurelia
   .register(
+    AppConfigurationPlugin,
     AureliaAuthConfiguration.configure(<IAuthConfigOptions>{responseTokenProp: 'bearerToken' }),
-    AppConfigurationPlugin.customize(
-      settings => {
-        settings.Dir = "config"
-        settings.File = "config.json"
-      }),
-    ApiPlugin.configure(
-      (reg) => {
-        //reg done in api beforeActivate hoo, from config.
-        //reg.registerEndpoint('lookupsApi', 'https://api.daas.selfip.net');
-        //reg.registerEndpoint('googleApi', 'https://www.cbbh.ba');
-      })
+    ApiPlugin
   ).app(MyApp)
   .start();
