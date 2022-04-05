8d9b075e39041085ab878d36cc644e7b4be3897f
Steffan_nj <stefannjegomirovic@gmail.com>
2022-04-05 20:16:31 +0200

    authService test

--- packages/au2-servicestack-auth/test/example.spec.ts
+++
@@ -1,8 +0,0 @@
-import {AuthService} from '../src/index'
-
-
-test('Shouldsign in', async () => {
-    const svc = new AuthService();
-    const rsp = await svc.signIn({username:"admin", password:"admin"});
-    expect(rsp.displayName).toBe("John");
-});
